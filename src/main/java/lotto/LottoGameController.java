a09e1a5b19b6a7660655451d1d74bdeedc91b050
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-30 03:41:25 +0900

    feat: 로또 구매 기능 구현
    - LottoStore: 구매 금액 검증 및 로또 발급 기능 구현
    - Controller: 금액 파싱, 예외 처리 및 로또 구매 흐름 제어feat: 로또 구매 기능 구현

---
+++ src/main/java/lotto/LottoGameController.java
@@ -0,0 +1,37 @@
+package lotto;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGameController {
+
+    private InputView inputView;
+    private OutputView outputView;
+    private LottoStore lottoStore;
+
+    LottoGameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.lottoStore = new LottoStore();
+    }
+
+    public void run() {
+        String inputMoney = inputView.inputPurchaseAmount();
+
+        int money = parseMoney(inputMoney);
+
+        List<Lotto> lottos = lottoStore.buyLottos(money);
+
+        outputView.printLottos(lottos, count);
+    }
+
+    private int parseMoney(String inputMoney) throws IllegalArgumentException{
+        try{
+            return Integer.parseInt(inputMoney);
+        }catch (NumberFormatException | NullPointerException e){
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
+        }
+    }
+}
