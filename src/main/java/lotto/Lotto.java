b5173870178e4394a56355391030bc89ba8442bb
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-31 01:04:10 +0900

    feat: 로또 번호 예외 처리 추가

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,7 +1,9 @@
 package lotto;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -15,9 +17,20 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        if (uniqueNumbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
+        }
+        // 3. 범위 검사
+        if (hasInvalidRange(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+        }
     }
 
-    // TODO: 추가 기능 구현
+    private boolean hasInvalidRange(List<Integer> numbers) {
+        return numbers.stream().anyMatch(n -> n < 1 || n > 45);
+    }
+
     @Override
     public String toString() {
         return numbers.toString();
