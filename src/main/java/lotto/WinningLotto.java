2a0bfc521b324666684dc2b69472b239734b2571
BaeYongho <dyddyd134@chungbuk.ac.kr>
2025-10-31 01:29:37 +0900

    feat: 당첨 로또 클래스 추가

---
+++ src/main/java/lotto/WinningLotto.java
@@ -0,0 +1,32 @@
+package lotto;
+
+import java.util.List;
+
+public class WinningLotto {
+
+    private final Lotto mainLotto;
+    private final int bonusNumber;
+
+    public WinningLotto(Lotto mainLotto, int bonusNumber) {
+        validateBonusNumber(mainLotto, bonusNumber);
+        this.mainLotto = mainLotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validateBonusNumber(Lotto mainLotto, int bonus) {
+        if (bonus < 1 || bonus > 45) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 1~45 사이여야 합니다.");
+        }
+        if (mainLotto.contains(bonus)) {
+            throw new IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public List<Integer> getMainNumbers() {
+        return mainLotto.getNumbers();
+    }
+
+    public int getBonusNumber() {
+        return bonusNumber;
+    }
+}
